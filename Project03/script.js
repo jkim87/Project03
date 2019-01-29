ba85072e0f6790367ce1cafca9407a65c1fafe4e
jkim87 <46460877+jkim87@users.noreply.github.com>
2019-01-29 13:33:15 -0500

    Add files via upload

--- Project03/script.js
+++ Project03/script.js
@@ -1,3 +1,8 @@
+
+
+
+
+
 var images = [
         './image/IMG_2596.png',
         './image/IMG_2126.png',
@@ -13,3 +18,26 @@ for (var j=images.length; j--;) {
 document.getElementById('myButton').addEventListener('click', function() {
     this.src = images[i >= images.length - 1 ? i = 0 : ++i];
 }, false);
+
+
+
+// class HoverButton {
+//   onHover(x, y) {
+//     TweenMax.to(this.el, 0.4, {
+//       x: (x - this.x) * 0.4,
+//       y: (y - this.y) * 0.4,
+//       scale: 1.15,
+//       ease: Power2.easeOut
+//     });
+//     this.el.style.zIndex = 10;
+//   }
+//   onLeave() {
+//     TweenMax.to(this.el, 0.7, {
+//       x: 0,
+//       y: 0,
+//       scale: 1,
+//       ease: Elastic.easeOut.config(1.2, 0.4)
+//     });
+//     this.el.style.zIndex = 1;
+//   }
+// }
